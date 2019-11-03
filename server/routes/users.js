9aacaa6001086143366f4e3f7a9cf1c42833f281
Gabriel Pérez <juan.gabriel.ps@gmail.com>
2019-11-03 20:24:07 -0300

    Creada la conexion mysql, añadido el body-parser, creado el metodo Post, testeado error si esta vacio

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
 const router = express.Router();
+
+const bodyParser = require('body-parser');
+
+const mysqlConnection = require('../dbConnection');
+let query = "";
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', (req, res) => {
   res.json({message: "welcome"})
+});
+
+router.post('/guardarPokemonsFavoritos', (req, res) => {
+  if(Object.keys(req.body).length != 0){
+    query = `CREATE TABLE IF NOT EXISTS favourites { 'user' varchar(50) NOT NULL,}`;
+    mysqlConnection.query();
+  }else{
+    return res.status(400).send("No se envio ninguna informacion");
+  }
 });
 
 module.exports = router;
