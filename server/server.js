9cfb1f33897c2aeba83490e94967f5937416580e
Gabriel Perez <juan.gabriel.ps@gmail.com>
2019-11-07 21:11:39 -0300

    Cambios hechos para ordenar mejor la interfaz, agregado gif para carga de imagenes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,19 @@ const express = require('express');
 const app = express();
 
 //express le dice al SO que escuche el puerto especificado y que si hay un evento http, lo informa
-app.listen(3000, () => {
-  console.log('listening over port 3000');
+// app.listen(3000, () => {
+//   console.log('listening over port 3000');
+// });
+
+//el process.env.PORT lo da la aplicacion, es decir, setea el puerto
+app.set('port', process.env.PORT || 3000); 
+
+app.use(require('./routes/users'));
+
+app.listen(app.get('port'), () => {
+  console.log(`Listening on ${app.get('port')}`);
 });
+
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
