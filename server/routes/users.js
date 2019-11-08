f7282eb3f0b172e2fa684902351b3d768b72add4
Gabriel Perez <juan.gabriel.ps@gmail.com>
2019-11-07 21:22:51 -0300

    Agregado cambios para mejorar el display, agregado load para carga de imagenes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,25 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
-
-const mysqlConnection = require('../dbConnection');
-let query = "";
-
-const bodyParser = require('body-parser');
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', (req, res) => {
   res.json({message: "welcome"})
-});
-
-router.post('/guardarPokemonsFavoritos', (req, res) => {
-  if(Object.keys(req.body).length != 0){
-    query = `CREATE TABLE IF NOT EXISTS favourites { 'user' varchar(50) NOT NULL,}`;
-    mysqlConnection.query();
-  }else{
-    return res.status(400).send("No se envio ninguna informacion");
-  }
 });
 
 module.exports = router;
