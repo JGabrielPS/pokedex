18b1741a1652b55ce06d1f8c35dae9708cf365f7
Gabriel Pérez <juan.gabriel.ps@gmail.com>
2019-11-10 18:44:49 -0300

    Cambios hechos para inicializar el primer post

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
 const router = express.Router();
+
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', (req, res) => {
   res.json({message: "welcome"})
 });
+
+router.post('/saveFavouritesPokemons', (req, res) => {
+  if(Object.keys(req.body).length != 0){
+    res.status(200).send("Objeto recibido")
+  }else{
+    res.status(400).send("Objeto vacio")
+  }
+})
 
 module.exports = router;
