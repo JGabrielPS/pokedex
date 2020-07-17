36c683873ac9aefde1ba582a65e1975a0272a4b9
Juan Gabriel Perez Salvatierra <juan.gabriel.ps@gmail.com>
2020-07-17 00:06:17 -0300

    Subject: Correcciones en auth por validaciones
    Issue N°: 9
    
    Description: Pequeñas correciones para que los datos de sesion sean eliminados cada vez que se pide la ruta y tambien los datos de contexto. Se debe crear un middleware para no repetir el codigo en register y login.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,22 +2,24 @@ const express = require("express");
 const router = express.Router();
 const redirectAuthenticated = require("../middleware/redirectAuthenticated");
 
-let context = {};
-
 router
   .get("/register", redirectAuthenticated, (req, res) => {
-    context.pageTitle = "Registro de usuario - Pokedex";
+    let context = {pageTitle: "Registro de usuario - Pokedex"};
+    console.log(req.session.errors, context);
     if (req.session.errors) {
       context.param = req.session.errors.param;
       context.msg = req.session.errors.msg;
+      delete req.session.errors;
     }
     res.render("register", context);
   })
   .get("/login", redirectAuthenticated, (req, res) => {
-    context.pageTitle = "Login de usuario - Pokedex";
+    let context = {pageTitle: "Login de usuario - Pokedex"};
+    console.log(req.session.errors, context);
     if (req.session.errors) {
       context.param = req.session.errors.param;
       context.msg = req.session.errors.msg;
+      delete req.session.errors;
     }
     res.render("login", context);
   })
